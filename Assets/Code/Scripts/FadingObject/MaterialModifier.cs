38fdd533131b51a441b9076377b51556e30e3cd5
Leon Nuredini <136099316+PlayerTtwo@users.noreply.github.com>
2024-05-05 09:52:29 +0200

    Finished working on the transparency effect

--- Assets/Code/Scripts/FadingObject/MaterialModifier.cs
+++ Assets/Code/Scripts/FadingObject/MaterialModifier.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MaterialModifier : MonoBehaviour
 {
@@ -11,11 +12,25 @@ public class MaterialModifier : MonoBehaviour
     private Material[] _transparentMaterialArray;
     private Material[] _selectionMaterialArray;
 
-
     private readonly string _highlightSOPath = "ScriptableObjects/Data/HighlightMaterialModifier";
 
     private bool _isTransparent;
     private bool _isSelected;
+
+    #region ShaderPropertyNames
+
+    private readonly string _alphaPremultiplyOn = "_ALPHAPREMULTIPLY_ON";
+    private readonly string _zWrite = "_ZWrite";
+    private readonly string _renderType = "RenderType";
+    private readonly string _transparent = "Transparent";
+    private readonly string _opaque = "Opaque";
+    private readonly string _color = "_Color";
+    private readonly string _dstBlend = "_DstBlend";
+    private readonly string _glossiness = "_Glossiness";
+    private readonly string _metallic = "_Metallic";
+    private readonly string _mode = "_Mode";
+
+    #endregion
 
     private void Awake()
     {
@@ -71,13 +86,47 @@ public class MaterialModifier : MonoBehaviour
         if (_isTransparent) return; 
         if (_isSelected) return; 
         _isTransparent = true;
-        _renderer.materials = _transparentMaterialArray; 
+        //_renderer.materials = _transparentMaterialArray; 
+
+        for (int i = 0; i < _renderer.materials.Length; i++)
+        {
+            Material renderMat = _renderer.materials[i];
+            renderMat.SetInt(_zWrite, 0);
+            renderMat.renderQueue = (int)RenderQueue.Transparent;
+            renderMat.renderQueue = 3000;
+            renderMat.SetOverrideTag(_renderType, _transparent);
+            Color fadedColor = renderMat.color;
+            fadedColor.a = 0f;
+            renderMat.color = fadedColor;
+            renderMat.EnableKeyword(_alphaPremultiplyOn);
+            renderMat.SetFloat(_dstBlend, 10);
+            renderMat.SetFloat(_metallic, 0.1f);
+            renderMat.SetFloat(_glossiness, .5f);
+            renderMat.SetInt(_mode, 3);
+        }
     }
 
     public void MakeOpaque() 
     {
         _isTransparent = false;
-        _renderer.materials = _materialArray;
+        //_renderer.materials = _materialArray;
+
+        for (int i = 0; i < _renderer.materials.Length; i++)
+        {
+            Material renderMat = _renderer.materials[i];
+            renderMat.SetInt(_zWrite, 1);
+            renderMat.renderQueue = (int)RenderQueue.Geometry;
+            renderMat.renderQueue = -1;
+            renderMat.SetOverrideTag(_renderType, _opaque);
+            Color opaqueColor = renderMat.color;
+            opaqueColor.a = 1f;
+            renderMat.color = opaqueColor;
+            renderMat.DisableKeyword(_alphaPremultiplyOn);
+            renderMat.SetFloat(_dstBlend, 0);
+            renderMat.SetFloat(_metallic, .9f);
+            renderMat.SetFloat(_glossiness, .6f);
+            renderMat.SetInt(_mode, 0);
+        }
     }
 
     public void MarkSelected()
